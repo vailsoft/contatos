33e02b7d25fc5eb5cfa65bc1c3ef808604681175
vailsoft <vailsoft@hotmail.com>
2021-08-30 10:51:49 -0300

    Acrescentado o componente Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ListaDeContatosComponent } from './components/lista-de-contatos/lista-de-contatos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ListaDeContatosComponent
   ],
   imports: [
     BrowserModule,
