d0cdaa3a3c14424557fb1509a6d9018e1cf72d7c
vailsoft <vailsoft@hotmail.com>
2021-08-30 12:08:36 -0300

    Criado o model contato e listandos contatos na lista de contatos sem os dados

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  contatos:Contato[] = [
+    {
+      nome: 'Mariana Ribeiro 1',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },
+    {
+      nome: 'Mariana Ribeiro 2',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },
+    {
+      nome: 'Mariana Ribeiro 3',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },
+    {
+      nome: 'Mariana Ribeiro 4',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },
+    {
+      nome: 'Mariana Ribeiro 5',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },
+    {
+      nome: 'Mariana Ribeiro 6',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },{
+      nome: 'Mariana Ribeiro 7',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },{
+      nome: 'Mariana Ribeiro 8',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },{
+      nome: 'Mariana Ribeiro 9',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },{
+      nome: 'Mariana Ribeiro 10',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },{
+      nome: 'Mariana Ribeiro 11',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    },{
+      nome: 'Mariana Ribeiro 12',
+      email: 'mariana@teste.com',
+      telefones:['1111-1111']
+    }
+  ]
 
   constructor() { }
 
