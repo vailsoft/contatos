c93deb876994120ce54db3e7bdfaecc7cb941bdd
vailsoft <vailsoft@hotmail.com>
2021-09-01 17:41:19 -0300

    Adicionando e removendo contatos no array de contatos

--- src/app/components/create-contato/create-contato.component.ts
+++ src/app/components/create-contato/create-contato.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Contato } from 'src/app/models/Contato';
+import { ContatoService } from 'src/app/services/contato.service';
 
 @Component({
   selector: 'app-create-contato',
@@ -9,12 +10,14 @@ import { Contato } from 'src/app/models/Contato';
 export class CreateContatoComponent implements OnInit {
 
   @Output() onCancelarClick:EventEmitter<null> = new EventEmitter();
-  
+
   novoContato:Contato = {
     nome:"Vailson Silva",
     email:"vailsoft@hotmail.com",
     telefones: [""]
   }
+
+  cs:ContatoService = new ContatoService();
 
   constructor() { }
 
@@ -38,6 +41,18 @@ export class CreateContatoComponent implements OnInit {
 
   removeTelefone(pos: number):void{
     this.novoContato.telefones.splice(pos, 1);
+  }
+
+  salvar(){
+      this.cs.addContato(this.novoContato);
+      //Sumir depois de cancelar
+      //this.onCancelarClick.emit();
+
+      this.novoContato = {
+        nome:"",
+        email: "",
+        telefones: [""]
+      }
   }
 
 }
