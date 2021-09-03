c50e4f228e766c0d2d51136c683bca049d4cbfc9
vailsoft <vailsoft@hotmail.com>
2021-09-03 15:54:39 -0300

    Salvando dados de contatos no localstore

--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -1,68 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Contato } from '../models/Contato';
 
-const BASE_DE_CONTATOS:Contato[] = [
-  {
-    nome: "Mariana Ribeiro 1",
-    email: "mariana@teste.com",
-    telefones: ['1111 - 1111','1111 - 1111','1111 - 1111']
-  },
-  {
-    nome: "Mariana Ribeiro 2",
-    email: "mariana@teste.com",
-    telefones: ['2222 - 2222']
-  },
-  {
-    nome: "Mariana Ribeiro 3",
-    email: "mariana@teste.com",
-    telefones: ['3333 - 3333']
-  },
-  {
-    nome: "Mariana Ribeiro 4",
-    email: "mariana@teste.com",
-    telefones: ['4444 - 4444']
-  },
-  {
-    nome: "Mariana Ribeiro 5",
-    email: "mariana@teste.com",
-    telefones: ['5555 - 5555']
-  },
-  {
-    nome: "Mariana Ribeiro 6",
-    email: "mariana@teste.com",
-    telefones: ['6666 - 6666']
-  },
-  {
-    nome: "Mariana Ribeiro 9",
-    email: "mariana@teste.com",
-    telefones: ['9999 - 9999']
-  },
-  {
-    nome: "Mariana Ribeiro 7",
-    email: "mariana@teste.com",
-    telefones: ['7777 - 7777']
-  },
-  {
-    nome: "Mariana Ribeiro 8",
-    email: "mariana@teste.com",
-    telefones: ['8888 - 8888']
-  },
-]
-
 @Injectable({
   providedIn: 'root'
 })
 export class ContatoService {
-
+  private readonly chave:string = "CONTATOS";
   constructor() { }
 
   getContatos():Contato[] {
-    console.log(Math.random())
-    return BASE_DE_CONTATOS;
+
+    // Tentar carregar os dados do localstorage
+    let dados = window.localStorage.getItem(this.chave);
+    // Verificar se havia dados no localstore
+    if(dados){
+      //Se houver dados transformar em array e retornar um array de contatos
+      let contatosCarregados:Contato[] = JSON.parse(dados);
+      return contatosCarregados;
+    } else {
+      // Se não houver dados, guardo o array no localstorage e retorna o array
+      window.localStorage.setItem(this.chave, "[]");
+      return [];
+    }
+
   }
 
   addContato(c:Contato): void {
-    BASE_DE_CONTATOS.push(c);
+    // Levantar os contatos do localstorage
+    let contatos = this.getContatos();
+    // adicionar o contato c o final do array
+    contatos.push(c);
+    // salvar o array de volta no localstorage
+    window.localStorage.setItem(this.chave, JSON.stringify(contatos));
   }
 
 }
