1bc779d7c591d7e48779111de04d58d3286864d7
Vailson dos Santos Silva <vailsoft>
2021-09-08 01:27:33 -0300

    Foi finalizado a parte de adicionar um contato e remover

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaDeContatosComponent } from './components/lista-de-contatos/lista-de-contatos.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListaDeContatosComponent,
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
